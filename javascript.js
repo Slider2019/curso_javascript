25bc1c6de8002d3b4b76a9ca2f052046c5d94971
Diego <46383501+Slider2019@users.noreply.github.com>
2024-03-19 23:19:31 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,17 @@
+/*
+Lenguaje de alto nivel
+Interpretado
+Dinamico
+Debilmente tipado
+Multiparadigma
+Sensible a Mayusculas y minusculas
+No necesitas los puntos y coma al final de cada linea
+*/
+
+/* 
+Usar UPPER_CASE en:
+
+constantes = const UNA_CONSTANTE
+
+
+*/
